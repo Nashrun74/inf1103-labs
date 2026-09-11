fb76779a4bcf630d61d36121faa46ab0cf583c60
Nashrun <2602966@sit.singaporetech.edu.sg>
2026-09-11 11:08:14 +0800

    First Commit

---
+++ auditor.py
@@ -0,0 +1,10 @@
+inventory = 0
+
+# while True:
+#     quantity = input("Enter stock quantity (or 'quit' to exit): ")
+
+#     if quantity.lower() == "quit":
+#         break
+
+#     print("Stock quantity entered:", quantity)
+
